e0bf5da6b9b018e5d15c7478c8fabeaafacadd45
DevashishParas <devashish.web@gmail.com>
2026-02-12 11:31:47 +0530

    new file

--- Lecture_20/boston_num.cpp
+++ Lecture_20/boston_num.cpp
@@ -18,7 +18,7 @@ using namespace std;        //digit
 bool prime(int n){
     for(int i=2;i*i<=n;i++){
         if(n%i==0){
-                return false;
+         return false;
         }
     }
     return true;
