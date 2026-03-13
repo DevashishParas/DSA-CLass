cce7570bb27d93c3a8570678b86b0808d2088be7
devashish.web@gmail.com <devashish.web@gmail.com>
2026-03-13 22:59:14 +0530

    leet

---
+++ Leetcode/pending_1678.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string interpret(string command) {
+
+        string ans="";
+        for(int i=0; i<command.size();){
+            if(command[i]=='G'){
+                ans.append("G");
+            i++;
+
+        }
+        else if(command.substr(i,2)=="()"){
+            ans.append("o");
+            i+=2;
+
+        }
+        else if(command.substr(i,4)=="(al)"){
+            ans.append("al");
+            i+=4;
+
+        }
+        
+        }
+        
+        return ans;
+    }
+
+};
