3a631d64122c2dceece4aecda890ff2f8ba5f95a
devashish.web@gmail.com <devashish.web@gmail.com>
2026-03-16 14:25:08 +0530

    leet

---
+++ Leetcode/8.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0, n=s.size();
+        //white spaces 
+
+        while (i<n and s[i] == ' '){
+            i++;
+            continue;
+
+
+        }
+        //sign ke liye check kare 
+        int sign =1;
+        if(i<n and (s[i]=='+' or s[i]=='-')){
+            if(s[i]=='-'){
+                sign= -1;
+
+            }
+            i++;
+
+        }
+        
+    //conversion
+    long long num=0;
+    while (i<n and isdigit(s[i])){
+        num=num*10+(s[i]-'0');
+        if(sign*num>INT_MAX){
+            return INT_MAX;
+
+        }
+        if(sign*num<INT_MIN){
+            return INT_MIN;
+
+        }
+        i++;
+
+    }
+    return sign*num;
+
+    }
+};
