2a15cf827567d83e3adbe19185f56067b16c3271
devashish.web@gmail.com <devashish.web@gmail.com>
2026-04-22 22:56:01 +0530

    leet

---
+++ Leetcode/1122.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int, int> freq;
+
+        // count frequency
+        for (int num : arr1) {
+            freq[num]++;
+        }
+
+        vector<int> result;
+
+        // place elements in order of arr2
+        for (int num : arr2) {
+            while (freq[num] > 0) {
+                result.push_back(num);
+                freq[num]--;
+            }
+        }
+
+        // collect remaining elements
+        vector<int> remaining;
+        for (auto &p : freq) {
+            while (p.second > 0) {
+                remaining.push_back(p.first);
+                p.second--;
+            }
+        }
+
+        sort(remaining.begin(), remaining.end());
+
+        // append remaining
+        result.insert(result.end(), remaining.begin(), remaining.end());
+
+        return result;
+    }
+};
