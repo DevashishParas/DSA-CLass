cefbc18d1252a0bc9cc45d12407e4aa0bbb31739
devashish.web@gmail.com <devashish.web@gmail.com>
2026-06-10 11:07:06 +0530

    leet

---
+++ Class_Full_Stack/Temp/merge.js
@@ -0,0 +1,12 @@
+const profiles = [
+   {
+      name:"Luffy",
+      role:"Marketing Manager",
+      image:"luffy.jpg"
+   },
+   {
+      name:"Zoro",
+      role:"Designer",
+      image:"zoro.jpg"
+   }
+];
